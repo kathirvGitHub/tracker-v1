851956d9842407126cb2a8f6540ff4b8c4df9214
Karthik Pandurangan <kpandurangan@FORZACONSULTING.local>
2017-07-16 23:45:31 +0200

    added socket functionality

--- public/tracker.js
+++ public/tracker.js
@@ -1,3 +1,72 @@
+var socket = io();
+
+var addButton = jQuery('#addToTimeLine');
+
+addButton.on('click', function () {
+    var personResptxt = jQuery('[name=timeline-personResp]');
+    var taskDonetxt = jQuery('[name=timeline-taskDone]');
+    var responseKeytxt = jQuery('[name=timeline-responseKey]');
+    var responseMessagetxt = jQuery('[name=timeline-responseMessage]');
+    var panelTypetxt = jQuery('[name=timeline-panelType]');
+    var panelModetxt = jQuery('[name=timeline-panelMode]');
+    var iconTypetxt = jQuery('[name=timeline-iconType]');
+    var divIDtxt = jQuery('[name=timeline-divID]');
+
+    var template = jQuery('#timeline-template').html();
+    var html = Mustache.render(template, {
+        personResp: personResptxt.val(),
+        taskDone: taskDonetxt.val(),
+        responseKey: responseKeytxt.val(),
+        responseMessage: responseMessagetxt.val(),
+        panelType: panelTypetxt.val(),
+        panelMode: panelModetxt.val(),
+        iconType: iconTypetxt.val(),
+        divID: divIDtxt.val()
+    });
+
+    console.log('Formed message', html);
+
+    var timelineHtml = jQuery('#timelineID');
+
+    // console.log('Current timeline ', timelineHtml.html());
+
+    timelineHtml.append(html);
+})
+
+var updateButton = jQuery('#updateTimeLine');
+
+updateButton.on('click', function () {
+    var divIDtxt = jQuery('[name=timeline-updateDivID]');
+    var personResptxt = jQuery('[name=timeline-personResp]');
+    var taskDonetxt = jQuery('[name=timeline-taskDone]');
+    var responseKeytxt = jQuery('[name=timeline-responseKey]');
+    var responseMessagetxt = jQuery('[name=timeline-responseMessage]');
+    var panelTypetxt = jQuery('[name=timeline-panelType]');
+    var panelModetxt = jQuery('[name=timeline-panelMode]');
+    var iconTypetxt = jQuery('[name=timeline-iconType]');
+
+    // var template = jQuery('#' + divIDtxt.val()).html();
+    var template = jQuery('#timeline-template').html();
+    var html = Mustache.render(template, {
+        personResp: personResptxt.val(),
+        taskDone: taskDonetxt.val(),
+        responseKey: responseKeytxt.val(),
+        responseMessage: responseMessagetxt.val(),
+        panelType: panelTypetxt.val(),
+        panelMode: panelModetxt.val(),
+        iconType: iconTypetxt.val(),
+        divID: divIDtxt.val()
+    });
+
+    console.log('Div ID will be updated to', html);
+
+    var timelineHtml = jQuery('#' + divIDtxt.val());
+
+    // // console.log('Current timeline ', timelineHtml.html());
+
+    timelineHtml.replaceWith(html);
+})
+
 jQuery('#timeline-form').on('submit', function (e) {
     e.preventDefault();
 
@@ -12,14 +81,14 @@ jQuery('#timeline-form').on('submit', function (e) {
 
     var template = jQuery('#timeline-template').html();
     var html = Mustache.render(template, {
-        personResp : personResptxt.val(),
-        taskDone : taskDonetxt.val(),
-        responseKey : responseKeytxt.val(),
-        responseMessage : responseMessagetxt.val(),
-        panelType : panelTypetxt.val(),
-        panelMode : panelModetxt.val(),
-        iconType : iconTypetxt.val(),
-        divID : divIDtxt.val()
+        personResp: personResptxt.val(),
+        taskDone: taskDonetxt.val(),
+        responseKey: responseKeytxt.val(),
+        responseMessage: responseMessagetxt.val(),
+        panelType: panelTypetxt.val(),
+        panelMode: panelModetxt.val(),
+        iconType: iconTypetxt.val(),
+        divID: divIDtxt.val()
     });
 
     console.log('Formed message', html);
@@ -43,18 +112,18 @@ jQuery('#timeline-updateForm').on('submit', function (e) {
     var panelTypetxt = jQuery('[name=timeline-panelType]');
     var panelModetxt = jQuery('[name=timeline-panelMode]');
     var iconTypetxt = jQuery('[name=timeline-iconType]');
-    
+
     // var template = jQuery('#' + divIDtxt.val()).html();
     var template = jQuery('#timeline-template').html();
     var html = Mustache.render(template, {
-        personResp : personResptxt.val(),
-        taskDone : taskDonetxt.val(),
-        responseKey : responseKeytxt.val(),
-        responseMessage : responseMessagetxt.val(),
-        panelType : panelTypetxt.val(),
-        panelMode : panelModetxt.val(),
-        iconType : iconTypetxt.val(),
-        divID : divIDtxt.val()
+        personResp: personResptxt.val(),
+        taskDone: taskDonetxt.val(),
+        responseKey: responseKeytxt.val(),
+        responseMessage: responseMessagetxt.val(),
+        panelType: panelTypetxt.val(),
+        panelMode: panelModetxt.val(),
+        iconType: iconTypetxt.val(),
+        divID: divIDtxt.val()
     });
 
     console.log('Div ID will be updated to', template);
@@ -67,4 +136,91 @@ jQuery('#timeline-updateForm').on('submit', function (e) {
 
 })
 
-    
+socket.on('connect', function () {
+    console.log('Connected to server');
+
+    var template = jQuery('#loadingTrackID-template').html();
+    var html = Mustache.render(template, {
+        divID: "initialTimeLineMessage"
+    });
+    var timelineHtml = jQuery('#timelineID');
+    timelineHtml.append(html);
+
+    var params = jQuery.deparam(window.location.search);
+
+    socket.emit('findTrack', params, function (err) {
+        if (err) {
+            // console.log(err);
+            template = jQuery('#invalidTrackID-template').html();
+            html = Mustache.render(template, {
+                divID: "initialTimeLineMessage"
+            });
+            // console.log('Div ID will be updated to', template);
+            timelineHtml = jQuery('#initialTimeLineMessage');
+            // console.log(timelineHtml.html());
+            timelineHtml.replaceWith(template);
+        } else {
+
+        }
+    });
+
+    // socket.emit('join', params, function(err) {
+    //     if(err){
+    //         alert(err);
+    //         window.location.href = '/';
+    //     }else{
+    //         console.log('No Error');
+    //     }
+    // })
+});
+
+socket.on('createTrack', function (trackInfo) {
+    var timelineHtml = jQuery('#timelineID');
+    timelineHtml.empty();
+    console.log(trackInfo);
+
+    timelineHtml.append("<div class='line text-muted'></div>");
+
+    trackInfo.forEach(function (trackDetail) {
+        var template = jQuery('#timeline-template').html();
+        var html = Mustache.render(template, {
+            personResp: trackDetail.personResp,
+            taskDone: trackDetail.taskDone,
+            responseKey: trackDetail.responseKey,
+            responseMessage: trackDetail.responseMessage,
+            panelType: trackDetail.panelType,
+            panelMode: trackDetail.panelMode,
+            iconType: trackDetail.iconType,
+            divID: trackDetail.divID
+        });
+
+        console.log('Formed message', html);
+        // console.log('Current timeline ', timelineHtml.html());
+        timelineHtml.append(html);
+    });
+});
+
+socket.on('updateTrackDetail', function (trackDetail) {
+    var template = jQuery('#timeline-template').html();
+    var html = Mustache.render(template, {
+        personResp: trackDetail.personResp,
+        taskDone: trackDetail.taskDone,
+        responseKey: trackDetail.responseKey,
+        responseMessage: trackDetail.responseMessage,
+        panelType: trackDetail.panelType,
+        panelMode: trackDetail.panelMode,
+        iconType: trackDetail.iconType,
+        divID: trackDetail.divID
+    });
+
+    var timelineHtml = jQuery('#' + trackDetail.divID);
+
+    // // console.log('Current timeline ', timelineHtml.html());
+
+    timelineHtml.replaceWith(html);
+});
+
+socket.on('disconnect', function () {
+    console.log('Server disconnected');
+});
+
--- server/server.js
+++ server/server.js
@@ -3,17 +3,167 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
+const { isRealString } = require('./utils/validation')
 
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
+
+io.on('connection', (socket) => {
+    console.log('client connected with ID:', socket.id);
+
+    socket.on('findTrack', (params, callback) => {
+        console.log(params);
+
+        if (!params || !params.trackID) {
+            console.log("Invalid Params");
+            return callback('Invalid Params');
+        }
+
+        console.log(params.trackID);
+
+        if (!isRealString(params.trackID)) {
+            return callback('Empty Track ID!');
+        }
+
+        //     var jdePassword = params.jdePassword;
+        //     var jdeUser = params.jdeUser;
+
+        //     /* New functionality to directly access the page from JDE, passing the user info as query string */
+
+        //     if (jdePassword && jdeUser) {
+        //         /* password and jdeUser exists, so create user profile dynamically since they would have not come via login screen */
+        //         users.removeUser(params.user);
+        //         users.addUser(jdeUser, jdePassword, params.user);
+        //     }
+
+        //     var user = users.getUser(params.user);
+
+        //     if (!user) {
+        //         return callback('User invalid/Session refreshed. Please login again!');
+        //     }
+
+        //     if (!isRealString(user.jdePassword)) {
+        //         users.removeUser(params.user);
+        //         return callback('Empty Password!');
+        //     }
+
+        //     // validate JDE login
+
+        //     var jdeLoginURL = `${JDEServerURL}/jderest/tokenrequest`;
+        //     var jdeLoginData = {
+        //         "username": user.jdeUserID,
+        //         "password": user.jdePassword,
+        //         "deviceName": "nodeJSServer",
+        //         "environment": "JDV910",
+        //         "role": "*ALL"
+        //     };
+
+        //     axios.post(jdeLoginURL, jdeLoginData).then((response) => {
+        //         // Logged in sucessfully
+        //         var jdeLogoutURL = `${JDEServerURL}/jderest/tokenrequest/logout`;
+        //         var jdeLogoutData = {
+        //             "token": response.data.userInfo.token
+        //         };
+
+        //         axios.post(jdeLogoutURL, jdeLogoutData).then((response) => {
+
+        //         });
+
+        //         users.updateSocketID(params.user, socket.id);
+        //         socket.emit('loggedIn');
+
+        //     }).catch((e) => {
+        //         console.log('Invalid User Credentials');
+        //         users.removeUser(user.jdeUserID);
+        //         socket.emit('invalidJDEUser');
+        //     })
+
+        var trackCreationData = [{
+            personResp: "Jan",
+            taskDone: "created Task 123",
+            responseKey: "Task 123",
+            responseMessage: "was created",
+            panelType: "panel-primary",
+            panelMode: "panel-success",
+            iconType: "glyphicon-ok",
+            divID: "1"
+        }, {
+            personResp: "Karthik",
+            taskDone: "created Task 123",
+            responseKey: "Task 123",
+            responseMessage: "was created",
+            panelType: "panel-primary",
+            panelMode: "panel-success",
+            iconType: "glyphicon-ok",
+            divID: "2"
+        }, {
+            personResp: "Zhivko",
+            taskDone: "created Task 123",
+            responseKey: "Task 123",
+            responseMessage: "was created",
+            panelType: "panel-primary",
+            panelMode: "panel-success",
+            iconType: "glyphicon-ok",
+            divID: "2"
+        }];
+        socket.emit('createTrack', trackCreationData);      
+
+        callback();
+    });
+
+    socket.on('getTrackUpdates', (params, callback) => {
+        var trackDetail = {
+            personResp: "Karthik",
+            taskDone: "created Task 123",
+            responseKey: "Task 123",
+            responseMessage: "was created",
+            panelType: "panel-primary",
+            panelMode: "panel-danger",
+            iconType: "glyphicon-cog",
+            divID: "2"
+        };
+
+        socket.emit('updateTrackDetail', trackDetail);    
+    });
+
+    // socket.on('getAvailabilityData', () => {
+    //     // var itemAvailabilityData = {
+    //     //     itemNames : ['Item Z', 'Item Y', 'Item X', 'Item W', 'Item V'],
+    //     //     itemAvailableNos : [getRandomInt(-100, 100), getRandomInt(-100, 100), getRandomInt(-100, 100), getRandomInt(-100, 100), getRandomInt(-100, 100)]
+    //     // };
+
+    //     // socket.emit('updateAvailabilityData', itemAvailabilityData);
+
+    //     // var itemAvailabilityData = {
+    //     //     itemNames : ['Item Z', 'Item Y', 'Item X', 'Item W', 'Item V'],
+    //     //     itemAvailableNos : [getRandomInt(-100, 100), getRandomInt(-100, 100), getRandomInt(-100, 100), getRandomInt(-100, 100), getRandomInt(-100, 100)]
+    //     // };
+
+    //     // socket.emit('updateAvailabilityData2', itemAvailabilityData);
+
+    //     var user = users.getUserBySocketID(socket.id);
+
+    //     if (user) {
+    //         getJDEAvailability(socket, user);
+    //     }
+
+    // });
+
+    socket.on('disconnect', () => {
+        // users.removeUserBySocket(socket.id);
+        console.log('Client disconnected');
+    })
+});
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
